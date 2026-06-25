6dd228abf7e1070966f691e1e04abc866bd7025f
mdfarhancom <mdfarhancom62@gmail.com>
2026-06-25 17:56:42 +0530

    add code in js

--- java.js
+++ java.js
@@ -1,17 +1,6 @@
-// // how to creat object;
-// let car={
-   // model: 'fortunure',
-   //year: 2015,
-   //color: 'white',
-   //}
-   //{
-   //concole.log (car.year);
-   //concole.log (car);
-   //concole.log (car.color);
-    //}
 
     let a= 10;
     let b= 10;
     let c= a+b ;
-    console.log(c);
+    console.log(a);
 
